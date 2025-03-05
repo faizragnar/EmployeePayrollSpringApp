27f71c8bab688ea48e2dfb734d0960141ae5d417
unknown <faiyazamail@gmail.com>
2025-03-05 17:04:07 +0530

    [Faiyaj ahmed] Add . connected SQL database

--- src/main/java/org/example/employeepayrollspringapp/model/Employee.java
+++ src/main/java/org/example/employeepayrollspringapp/model/Employee.java
@@ -1,7 +1,11 @@
 package org.example.employeepayrollspringapp.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -16,41 +20,4 @@ public class Employee {
 
     public Employee() {}
 
-    public Employee(String name, String department, double salary) {
-        this.name = name;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
